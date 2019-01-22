0b2f7c9e2d4ff6743dcd1be227386843f672c083
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2019-01-22 15:50:32 -0500

    Sum row aggregate table

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,6 +4,7 @@ import { Icon, Label, Menu, Table, Button } from 'semantic-ui-react';
 const Project = props => {
   const { title, id } = props.project;
   const handleDeleteProject = props.handleDeleteProject;
+  const timers = props.timers;
   return (
     <Table.Row>
       <Table.Cell>{title}</Table.Cell>
--- src/components/TimerList.js
+++ src/components/TimerList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import TimerDetail from './TimerDetail';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react';
+import { Icon, Label, Menu, Table, TransitionablePortal } from 'semantic-ui-react';
 import { Tab } from '@material-ui/core';
 
 const TimerList = props => {
   const timers = props.timers;
   const removeTimer = props.removeTimer;
   const handleDeleteTimer = props.handleDeleteTimer;
+
+  const timerSum = () => {
+    let total = 0;
+    timers.forEach(timer => {
+      total += parseInt(timer.seconds);
+    });
+    return total;
+  };
   return (
     <div>
       <h3>Active Timers</h3>
@@ -28,6 +36,10 @@ const TimerList = props => {
               handleDeleteTimer={handleDeleteTimer}
             />
           ))}
+          <Table.Row>
+            <Table.Cell>Total Duration</Table.Cell>
+            <Table.Cell>{timerSum()} seconds</Table.Cell>
+          </Table.Row>
           <Table.Row />
         </Table.Body>
       </Table>
