50dfb25ace42b13d21282142dabbcf288c9971c9
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2019-01-24 20:35:15 -0500

    much you have to learn young padawan

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,11 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Home Page</h1>
+        <h1>Chungus Time</h1>
+        <img
+          src="https://vignette.wikia.nocookie.net/weykipedia/images/7/76/Ugandan_Chungus.png/revision/latest?cb=20190101020540"
+          alt="ugandan chungus"
+        />
       </div>
     );
   }
--- src/components/TimerList.js
+++ src/components/TimerList.js
@@ -1,10 +1,10 @@
-import React from "react";
-import TimerDetail from "./TimerDetail";
-import ReactTable from "react-table";
-import { Icon, Label, Menu, Table } from "semantic-ui-react";
-import _ from "lodash";
-import Moment from "react-moment";
-import moment from "moment";
+import React from 'react';
+import TimerDetail from './TimerDetail';
+import ReactTable from 'react-table';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react';
+import _ from 'lodash';
+import Moment from 'react-moment';
+import moment from 'moment';
 import {
   LineChart,
   Line,
@@ -16,7 +16,7 @@ import {
   YAxis,
   Tooltip,
   Legend
-} from "recharts";
+} from 'recharts';
 // import { Tab } from "@material-ui/core";
 
 const TimerList = props => {
@@ -35,25 +35,19 @@ const TimerList = props => {
       },
       0
     ),
-    "seconds"
+    'seconds'
   );
 
-  const formattedDurationColumnTotal = durationColumnTotal.format("hh:mm:ss");
+  const formattedDurationColumnTotal = durationColumnTotal.format('hh:mm:ss');
   return (
     <div>
       <h3>Active Timers</h3>
-      <BarChart width={1920} height={600} data={timers}>
+      <BarChart width={1920} height={1080} data={timers}>
         <Tooltip />
         <Legend />
         <XAxis dataKey="title" />
         <YAxis />
-        <Bar
-          name="Duration (secs)"
-          type="monotone"
-          dataKey="seconds"
-          barSize={30}
-          fill="#1F85D0"
-        />
+        <Bar name="Duration (secs)" type="monotone" dataKey="seconds" barSize={30} fill="#1F85D0" />
       </BarChart>
       <Table sortable celled color="blue">
         <Table.Header>
@@ -62,7 +56,7 @@ const TimerList = props => {
               Timer <Icon name="tasks" />
             </Table.HeaderCell>
             <Table.HeaderCell>
-              Duration
+              Duration (hh:mm:ss)
               <Icon name="time" />
             </Table.HeaderCell>
             <Table.HeaderCell>
